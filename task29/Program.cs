de17790a263ae488a649ce1a15a8d04633ac2faf
Mikhail Nikorkin <nikorkin1990@mail.ru>
2022-10-13 11:10:52 +0300

    create task29

---
+++ task29/Program.cs
@@ -0,0 +1,24 @@
+﻿const int Size = 8;
+int[] ourArray = new int[Size];
+FillArray(ourArray, Size);
+PrintArray(ourArray, Size);
+
+
+void FillArray(int[] arr, int size)
+{
+
+    for (int i = 0; i < size; i++)
+    {
+        int a = new Random().Next(0, 100);
+        arr[i] = a;
+
+    }
+}
+
+void PrintArray(int[] arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
